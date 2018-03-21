33cba0981c6f43f72c21968b16d8e96182333f20
seeminglySafe <seeminglysafe@gmail.com>
2018-03-21 12:00:36 +0100

    AddContact Activity creatred

--- app/src/main/java/com/example/themichalkozak/contact/ListContact.java
+++ app/src/main/java/com/example/themichalkozak/contact/ListContact.java
@@ -3,7 +3,7 @@ package com.example.themichalkozak.contact;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ContactList extends AppCompatActivity {
+public class ListContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
