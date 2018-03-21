c797681159cb2207a246fbc150984a91b7137961
seeminglySafe <seeminglysafe@gmail.com>
2018-03-21 11:22:58 +0100

    ContactArrayList changed

--- app/src/main/java/com/example/themichalkozak/contact/ContactArrayList.java
+++ app/src/main/java/com/example/themichalkozak/contact/ContactArrayList.java
@@ -1,8 +1,30 @@
 package com.example.themichalkozak.contact;
 
+import java.util.ArrayList;
+
 /**
  * Created by themichalkozak on 21/03/2018.
  */
 
 public class ContactArrayList {
+
+    private String myName;
+    private ArrayList<Contact> contactArrayList;
+
+    public ContactArrayList(String myName, ArrayList<Contact> contactArrayList) {
+        this.myName = myName;
+        this.contactArrayList = new ArrayList<Contact>();
+    }
+
+    public boolean addContact(Contact contact){
+        if(findContact(contact)<0){
+            return false;
+        }
+        this.contactArrayList.add(contact);
+        return true;
+    }
+
+    private int findContact(Contact contact){
+        return this.contactArrayList.indexOf(contact);
+    }
 }
