24968084cc35858b13a37872399187cb2726cd3f
seeminglySafe <seeminglysafe@gmail.com>
2018-03-21 10:33:10 +0100

    contact object created.

--- app/src/main/java/com/example/themichalkozak/contact/Contact.java
+++ app/src/main/java/com/example/themichalkozak/contact/Contact.java
@@ -1,8 +1,30 @@
 package com.example.themichalkozak.contact;
 
+import java.util.logging.StreamHandler;
+
 /**
  * Created by themichalkozak on 21/03/2018.
  */
 
 public class Contact {
+
+    private String myName;
+    private String myPhoneNumber;
+
+    public Contact(String myName, String myPhoneNumber) {
+        this.myName = myName;
+        this.myPhoneNumber = myPhoneNumber;
+    }
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public String getMyPhoneNumber() {
+        return myPhoneNumber;
+    }
+
+    public Contact createNewContact(String myName, String myPhoneNumber){
+        return new Contact(myName,myPhoneNumber);
+    }
 }
