525bf49f37dc823ea7a1625a517df410e002351d
seeminglySafe <seeminglysafe@gmail.com>
2018-03-21 10:44:13 +0100

    ContactArrayList created

---
+++ app/src/main/java/com/example/themichalkozak/contact/ContactAdapter.java
@@ -0,0 +1,13 @@
+package com.example.themichalkozak.contact;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by themichalkozak on 21/03/2018.
+ */
+
+public class ContactAdapter  {
+
+    private String myName;
+    private ArrayAdapter<Contact> contactArrayAdapter;
+}
